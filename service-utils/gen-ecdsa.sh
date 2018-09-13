d3152d37d0eded49cabc6dd03bb4366739a738fc
lekamj <hannar.j.lee@gmail.com>
2018-09-13 16:20:00 -0400

    copy required files from account

---
+++ service-utils/gen-ecdsa.sh
@@ -0,0 +1,24 @@
+path_to_etc=$1
+
+mkdir tmp && cd tmp
+
+echo "Setting up credential directory structure..."
+mkdir -p $path_to_etc/creds/services/keys
+mkdir -p $path_to_etc/creds/services/certs
+
+for server_name in "${@:2}"
+do
+    echo "Generating ECDSA private key..."
+    openssl ecparam -name secp256k1 -genkey -noout -out $server_name-signing-key.pem
+    cp $server_name-signing-key.pem $path_to_etc/creds/services/keys
+
+    echo "Generating ECDSA public key..."
+    openssl ec -in $server_name-signing-key.pem -pubout -out $server_name-signing-pubkey.pem
+    cp $server_name-signing-pubkey.pem $path_to_etc/creds/services/certs
+done
+
+
+
+echo "Cleaning up..."
+cd ..
+rm -rf tmp
---
+++ service-utils/gen-ssl.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+path_to_etc=$1
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Setting up credential directory structure..."
+mkdir -p $path_to_etc/creds/root/certs
+mkdir -p $path_to_etc/creds/root/keys
+mkdir -p $path_to_etc/creds/services/certs
+mkdir -p $path_to_etc/creds/services/keys
+
+mkdir tmp && cd tmp
+
+echo "Generating root certs..."
+openssl genrsa -passout pass:1111 -des3 -out ca.key.pem 4096
+
+openssl req -passin pass:1111 -new -x509 -days 7300 -key ca.key.pem -out ca.cert.pem -subj  "/C=FR/ST=Paris/L=Paris/O=Test/OU=Test/CN=root"
+
+echo "Generating intermediate certs..."
+openssl genrsa -passout pass:1111 -des3 -out ca-intermediate.key.pem 4096
+
+openssl req -passin pass:1111 -new -key ca-intermediate.key.pem -out ca-intermediate.csr -subj  "/C=FR/ST=Paris/L=Paris/O=Test/OU=Test/CN=intermediate"
+
+openssl x509 -req -passin pass:1111 -days 365 -extensions v3_intermediate_ca -extfile $dir/openssl.conf -in ca-intermediate.csr -CA ca.cert.pem -CAkey ca.key.pem -set_serial 01 -out ca-intermediate.cert.pem
+
+openssl rsa -passin pass:1111 -in ca-intermediate.key.pem -out ca-intermediate.key.pem
+
+cp ca-intermediate.key.pem $path_to_etc/creds/root/keys/ca-intermediate.key.pem
+cp ca.key.pem $path_to_etc/creds/root/keys/ca.key.pem
+cp ca.cert.pem  $path_to_etc/creds/root/certs/ca.cert.pem
+cp ca-intermediate.cert.pem  $path_to_etc/creds/root/certs/ca-intermediate.cert.pem
+
+echo "Generating service certs..."
+for server_name in "${@:2}"
+do
+    openssl genrsa -passout pass:1111 -des3 -out $server_name.key.pem 4096
+
+    openssl req -passin pass:1111 -new -key $server_name.key.pem -out $server_name.csr -subj  "/C=FR/ST=Paris/L=Paris/O=Test/OU=Test/CN=${server_name}.local.clicktherapeutics.com"
+
+    openssl x509 -req -passin pass:1111 -days 365 -in $server_name.csr -CA ca-intermediate.cert.pem -CAkey ca-intermediate.key.pem -set_serial 01 -out $server_name.cert.pem
+
+    openssl rsa -passin pass:1111 -in $server_name.key.pem -out $server_name.key.pem
+
+    echo "copying files to /etc/creds dir"
+    cat $server_name.cert.pem ca-intermediate.cert.pem ca.cert.pem > $path_to_etc/creds/services/certs/$server_name-chain.cert.pem
+    cp $server_name.key.pem $path_to_etc/creds/services/keys
+done
+
+echo "Cleaning up..."
+cd ..
+rm -rf tmp
