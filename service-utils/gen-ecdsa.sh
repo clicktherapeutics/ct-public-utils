59cc593542c99070ec444a6f000c972912da7fb3
lekamj <hannar.j.lee@gmail.com>
2018-09-14 11:04:17 -0400

    create setup script as well as hard code relative directory

--- service-utils/gen-ecdsa.sh
+++ service-utils/gen-ecdsa.sh
@@ -1,4 +1,4 @@
-path_to_etc=$1
+path_to_etc=/etc
 
 mkdir tmp && cd tmp
 
@@ -6,8 +6,9 @@ echo "Setting up credential directory structure..."
 mkdir -p $path_to_etc/creds/services/keys
 mkdir -p $path_to_etc/creds/services/certs
 
-for server_name in "${@:2}"
+for server_name in "${@:1}"
 do
+  echo $server_name
     echo "Generating ECDSA private key..."
     openssl ecparam -name secp256k1 -genkey -noout -out $server_name-signing-key.pem
     cp $server_name-signing-key.pem $path_to_etc/creds/services/keys
